46dbc17bf982cb70f32dedbf61de10216a00e50a
Jose Diaz-Nunez <josediaznunez3@gmail.com>
2024-03-10 14:40:51 -0500

    add standard input reading feature and update tests

--- cmd/wc-tool/main.go
+++ cmd/wc-tool/main.go
@@ -3,12 +3,15 @@ package main
 import (
 	"flag"
 	"fmt"
+	"io"
 	"os"
 
 	"wc-tool/pkg/commands"
 )
 
 func main() {
+	var input string
+	var file string
 	countLines := flag.Bool("l", false, "Count lines")
 	countWords := flag.Bool("w", false, "Count words")
 	countBytes := flag.Bool("c", false, "Count bytes")
@@ -16,37 +19,47 @@ func main() {
 
 	flag.Parse()
 
-	var input string
-	var filePath string
 	if flag.NArg() > 0 {
-		for _, file := range flag.Args() {
-			filePath = file
-			content, err := os.ReadFile(file)
-			if err != nil {
-				fmt.Fprintf(os.Stderr, "Error reading file %s: %v\n", file, err)
-				print(os.Getwd())
-				os.Exit(1)
-			}
-			input += string(content)
-		}
+		file = flag.Arg(0)
+		input = readFile(file)
+	} else {
+		input = readStandardInput()
 	}
 
 	if *countLines {
 		lines := commands.LineCount(input)
-		fmt.Printf("%d %s\n", lines, filePath)
+		fmt.Printf("\t%d %s\n", lines, file)
 	} else if *countWords {
 		words := commands.WordCount(input)
-		fmt.Printf("%d %s\n", words, filePath)
+		fmt.Printf("\t%d %s\n", words, file)
 	} else if *countBytes {
 		bytes := commands.ByteCount(input)
-		fmt.Printf("%d %s\n", bytes, filePath)
+		fmt.Printf("\t%d %s\n", bytes, file)
 	} else if *countCharacters {
 		characters := commands.CharacterCount(input)
-		fmt.Printf("%d %s\n", characters, filePath)
+		fmt.Printf("\t%d %s\n", characters, file)
 	} else {
 		lines := commands.LineCount(input)
 		bytes := commands.ByteCount(input)
 		words := commands.WordCount(input)
-		fmt.Printf("%d %d %d %s\n", lines, words, bytes, filePath)
+		fmt.Printf("\t%d \t%d \t%d \t%s\n", lines, words, bytes, file)
 	}
+}
+
+func readFile(file string) string {
+	content, err := os.ReadFile(file)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error reading from file %s: %v\n", file, err)
+		os.Exit(1)
+	}
+	return string(content)
+}
+
+func readStandardInput() string {
+	content, err := io.ReadAll(os.Stdin)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error reading from standard input: %v\n", err)
+		os.Exit(1)
+	}
+	return string(content)
 }
--- test/integration/wc-tool_integration_test.go
+++ test/integration/wc-tool_integration_test.go
@@ -6,7 +6,258 @@ import (
 	"testing"
 )
 
-func setup(t *testing.T) {
+func TestWCToolByteCountFromFile(t *testing.T) {
+	setupWCTool(t)
+
+	file := "./../test.txt"
+
+	cmd := exec.Command("./wc-tool", "-c", file)
+	output, err := cmd.CombinedOutput()
+	if err != nil {
+		cleanupWCTool(t)
+		t.Fatalf("Failed to execute wc-tool: %v", err)
+	}
+
+	expected := "\t342190" + " " + file + "\n"
+	if string(output) != expected {
+		t.Errorf("Expected output: %q, but got: %q", expected, output)
+	}
+
+	cleanupWCTool(t)
+}
+
+func TestWCToolLineCountFromFile(t *testing.T) {
+	setupWCTool(t)
+
+	file := "./../test.txt"
+
+	cmd := exec.Command("./wc-tool", "-l", file)
+	output, err := cmd.CombinedOutput()
+	if err != nil {
+		t.Fatalf("Failed to execute wc-tool: %v", err)
+	}
+
+	expected := "\t7145" + " " + file + "\n"
+	if string(output) != expected {
+		t.Errorf("Expected output: %q, but got: %q", expected, output)
+	}
+
+	cleanupWCTool(t)
+}
+
+func TestWCToolWordCountFromFile(t *testing.T) {
+	setupWCTool(t)
+
+	file := "./../test.txt"
+
+	cmd := exec.Command("./wc-tool", "-w", file)
+	output, err := cmd.CombinedOutput()
+	if err != nil {
+		t.Fatalf("Failed to execute wc-tool: %v", err)
+	}
+
+	expected := "\t58164" + " " + file + "\n"
+	if string(output) != expected {
+		t.Errorf("Expected output: %q, but got: %q", expected, output)
+	}
+
+	cleanupWCTool(t)
+}
+
+func TestWCToolCharacterCountFromFile(t *testing.T) {
+	setupWCTool(t)
+
+	file := "./../test.txt"
+
+	cmd := exec.Command("./wc-tool", "-m", file)
+	output, err := cmd.CombinedOutput()
+	if err != nil {
+		t.Fatalf("Failed to execute wc-tool: %v", err)
+	}
+
+	expected := "\t339292" + " " + file + "\n"
+	if string(output) != expected {
+		t.Errorf("Expected output: %q, but got: %q", expected, output)
+	}
+
+	cleanupWCTool(t)
+}
+
+func TestWCToolDefaultCountFromFile(t *testing.T) {
+	setupWCTool(t)
+
+	file := "./../test.txt"
+
+	cmd := exec.Command("./wc-tool", file)
+	output, err := cmd.CombinedOutput()
+	if err != nil {
+		t.Fatalf("Failed to execute wc-tool: %v", err)
+	}
+
+	expected := "\t7145 \t58164 \t342190" + " \t" + file + "\n"
+	if string(output) != expected {
+		t.Errorf("Expected output: %q, but got: %q", expected, output)
+	}
+
+	cleanupWCTool(t)
+}
+
+func TestWCToolLineCountFromStandardInput(t *testing.T) {
+	setupWCTool(t)
+
+	file := "./../test.txt"
+
+	content := read(file, t)
+
+	pipeReader, pipeWriter, err := os.Pipe()
+	if err != nil {
+		t.Fatalf("Error creating pipe: %v", err)
+	}
+	defer pipeReader.Close()
+	defer pipeWriter.Close()
+
+	write(pipeWriter, content, t)
+
+	cmd := exec.Command("./wc-tool", "-l")
+	cmd.Stdin = pipeReader
+	output, err := cmd.CombinedOutput()
+	if err != nil {
+		t.Fatalf("Failed to execute wc-tool: %v", err)
+	}
+
+	expected := "\t7145" + " " + "\n"
+	if string(output) != expected {
+		t.Errorf("Expected output: %q, but got: %q", expected, output)
+	}
+
+	cleanupWCTool(t)
+}
+
+func TestWCToolWordCountFromStandardInput(t *testing.T) {
+	setupWCTool(t)
+
+	file := "./../test.txt"
+
+	content := read(file, t)
+
+	pipeReader, pipeWriter, err := os.Pipe()
+	if err != nil {
+		t.Fatalf("Error creating pipe: %v", err)
+	}
+	defer pipeReader.Close()
+	defer pipeWriter.Close()
+
+	write(pipeWriter, content, t)
+
+	cmd := exec.Command("./wc-tool", "-w")
+	cmd.Stdin = pipeReader
+	output, err := cmd.CombinedOutput()
+	if err != nil {
+		t.Fatalf("Failed to execute wc-tool: %v", err)
+	}
+
+	expected := "\t58164" + " " + "\n"
+	if string(output) != expected {
+		t.Errorf("Expected output: %q, but got: %q", expected, output)
+	}
+
+	cleanupWCTool(t)
+}
+
+func TestWCToolByteCountFromStandardInput(t *testing.T) {
+	setupWCTool(t)
+
+	file := "./../test.txt"
+
+	content := read(file, t)
+
+	pipeReader, pipeWriter, err := os.Pipe()
+	if err != nil {
+		t.Fatalf("Error creating pipe: %v", err)
+	}
+	defer pipeReader.Close()
+	defer pipeWriter.Close()
+
+	write(pipeWriter, content, t)
+
+	cmd := exec.Command("./wc-tool", "-c")
+	cmd.Stdin = pipeReader
+	output, err := cmd.CombinedOutput()
+	if err != nil {
+		t.Fatalf("Failed to execute wc-tool: %v", err)
+	}
+
+	expected := "\t342190" + " " + "\n"
+	if string(output) != expected {
+		t.Errorf("Expected output: %q, but got: %q", expected, output)
+	}
+
+	cleanupWCTool(t)
+}
+
+func TestWCToolCharacterCountFromStandardInput(t *testing.T) {
+	setupWCTool(t)
+
+	file := "./../test.txt"
+
+	content := read(file, t)
+
+	pipeReader, pipeWriter, err := os.Pipe()
+	if err != nil {
+		t.Fatalf("Error creating pipe: %v", err)
+	}
+	defer pipeReader.Close()
+	defer pipeWriter.Close()
+
+	write(pipeWriter, content, t)
+
+	cmd := exec.Command("./wc-tool", "-m")
+	cmd.Stdin = pipeReader
+	output, err := cmd.CombinedOutput()
+	if err != nil {
+		t.Fatalf("Failed to execute wc-tool: %v", err)
+	}
+
+	expected := "\t339292" + " " + "\n"
+	if string(output) != expected {
+		t.Errorf("Expected output: %q, but got: %q", expected, output)
+	}
+
+	cleanupWCTool(t)
+}
+
+func TestWCToolDefaultCountFromStandardInput(t *testing.T) {
+	setupWCTool(t)
+
+	file := "./../test.txt"
+
+	content := read(file, t)
+
+	pipeReader, pipeWriter, err := os.Pipe()
+	if err != nil {
+		t.Fatalf("Error creating pipe: %v", err)
+	}
+	defer pipeReader.Close()
+	defer pipeWriter.Close()
+
+	write(pipeWriter, content, t)
+
+	cmd := exec.Command("./wc-tool")
+	cmd.Stdin = pipeReader
+	output, err := cmd.CombinedOutput()
+	if err != nil {
+		t.Fatalf("Failed to execute wc-tool: %v", err)
+	}
+
+	expected := "\t7145 \t58164 \t342190" + " \t\n"
+	if string(output) != expected {
+		t.Errorf("Expected output: %q, but got: %q", expected, output)
+	}
+
+	cleanupWCTool(t)
+}
+
+func setupWCTool(t *testing.T) {
 	buildCmd := exec.Command("go", "build", "-o", "wc-tool", "./../../cmd/wc-tool/main.go")
 	err := buildCmd.Run()
 	if err != nil {
@@ -14,86 +265,27 @@ func setup(t *testing.T) {
 	}
 }
 
-func cleanup(t *testing.T) {
+func cleanupWCTool(t *testing.T) {
 	err := os.Remove("wc-tool")
 	if err != nil {
 		t.Fatalf("Failed to remove wc-tool executable: %v", err)
 	}
 }
 
-func TestWCToolByteCount(t *testing.T) {
-	setup(t)
-
-	testFilePath := "./../test.txt"
-
-	cmd := exec.Command("./wc-tool", "-c", testFilePath)
-	output, err := cmd.CombinedOutput()
+func read(file string, t *testing.T) []byte {
+	content, err := os.ReadFile(file)
 	if err != nil {
-		cleanup(t)
-		t.Fatalf("Failed to execute wc-tool: %v", err)
+		t.Fatalf("Error reading test file: %v", err)
 	}
-
-	expected := "342190" + " " + testFilePath + "\n"
-	if string(output) != expected {
-		t.Errorf("Expected output: %q, but got: %q", expected, output)
-	}
-
-	cleanup(t)
+	return content
 }
 
-func TestWCToolLineCount(t *testing.T) {
-	setup(t)
-
-	testFilePath := "./../test.txt"
-
-	cmd := exec.Command("./wc-tool", "-l", testFilePath)
-	output, err := cmd.CombinedOutput()
-	if err != nil {
-		t.Fatalf("Failed to execute wc-tool: %v", err)
-	}
-
-	expected := "7145" + " " + testFilePath + "\n"
-	if string(output) != expected {
-		t.Errorf("Expected output: %q, but got: %q", expected, output)
-	}
-
-	cleanup(t)
-}
-
-func TestWCToolWordCount(t *testing.T) {
-	setup(t)
-
-	testFilePath := "./../test.txt"
-
-	cmd := exec.Command("./wc-tool", "-w", testFilePath)
-	output, err := cmd.CombinedOutput()
-	if err != nil {
-		t.Fatalf("Failed to execute wc-tool: %v", err)
-	}
-
-	expected := "58164" + " " + testFilePath + "\n"
-	if string(output) != expected {
-		t.Errorf("Expected output: %q, but got: %q", expected, output)
-	}
-
-	cleanup(t)
-}
-
-func TestWCToolCharacterCount(t *testing.T) {
-	setup(t)
-
-	testFilePath := "./../test.txt"
-
-	cmd := exec.Command("./wc-tool", "-m", testFilePath)
-	output, err := cmd.CombinedOutput()
-	if err != nil {
-		t.Fatalf("Failed to execute wc-tool: %v", err)
-	}
-
-	expected := "339292" + " " + testFilePath + "\n"
-	if string(output) != expected {
-		t.Errorf("Expected output: %q, but got: %q", expected, output)
-	}
-
-	cleanup(t)
+func write(pipeWriter *os.File, content []byte, t *testing.T) {
+	go func() {
+		defer pipeWriter.Close()
+		_, err := pipeWriter.Write(content)
+		if err != nil {
+			t.Errorf("Error writing to pipe: %v", err)
+		}
+	}()
 }
